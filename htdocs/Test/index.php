74c8bb8c665f57a56c21bdf12270454b60f0ff2c
Zeplin108 <zach_packer@yahoo.com>
2019-11-13 14:23:51 -0500

    modified:   bin/apache/logs/httpd.pid 	modified:   conf/php7.3.7/php.ini 	modified:   db/mysql/ib_buffer_pool 	modified:   db/mysql/ib_logfile0 	modified:   db/mysql/ibdata1 	new file:   htdocs/Test/index.php 	modified:   logs/access.log 	modified:   logs/apache_error.log 	modified:   logs/debug.log 	modified:   logs/mampapp.log 	modified:   logs/mysql_error_log.err 	modified:   logs/php_error.log

---
+++ htdocs/Test/index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	</head>
+	<body>
+	<h1>Test Page</h1>
+		<p>
+		<?php
+		   echo "Hi there.\n";
+		   $answer = 6 * 7;
+		   echo "The answer is $answer, what was the question again?\n";
+		?>
+		</p>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit odio ac nisi mollis sodales. Maecenas lobortis urna nibh, ac iaculis leo feugiat a. Proin viverra arcu in nibh accumsan, sit amet accumsan nisi maximus. Nulla eu diam quis orci bibendum scelerisque. Mauris tincidunt eros diam, eu fringilla mi egestas at. Etiam sollicitudin gravida nibh. Mauris purus neque, sollicitudin sed tortor ut, ultricies aliquet dui. Phasellus vel dictum risus. Phasellus et orci purus. Nunc quis ante ultricies, commodo nisl a, pellentesque sem. Pellentesque vulputate enim ac ornare ornare. Curabitur dapibus sapien nunc, a semper est iaculis a.
+
+Nunc ante diam, dignissim sed sodales ut, viverra vitae diam. Morbi et finibus velit, quis tincidunt velit. Donec auctor, magna id ultrices blandit, odio justo malesuada metus, non varius enim erat eu felis. Aenean fringilla quam quis elit ultricies imperdiet. Pellentesque eu arcu in urna congue pellentesque. Aliquam eleifend nisi et ultricies venenatis. Nullam lobortis scelerisque augue nec egestas. Mauris ut mollis nisl, a fringilla ante. Donec ultricies urna ac maximus gravida. Sed dui odio, venenatis id imperdiet quis, semper id justo. Mauris lobortis vitae metus a ullamcorper. .</p>
+	</body>
+</html>
