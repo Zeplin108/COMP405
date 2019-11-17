6b3b5c1ddfec3f3c988cd7a17d478cb27a187df7
pbauer21 <56447241+pbauer21@users.noreply.github.com>
2019-11-17 14:10:50 -0500

    Add files via upload

---
+++ htdocs/Webpages/LoginPage.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Consumables</title>
+    <link href="LandingPage.css" type="text/css" rel="Stylesheet" id="currstyle"/>
+  </head>
+
+  <body>
+    <div class="content">
+      <!-- Nav bar logic -->
+      <div class="topnav">
+        <a href="LandingPage.php">Home</a>
+        <a href="ExplorePage.php">Explore</a>
+        <a href="ReviewsPage.php">Reviews</a>
+        <a href="AboutPage.php">About</a>
+        <a class="active" href="LoginPage.php">Login</a>
+      </div>
+
+      <!-- TODO: fix styling -->
+      <div style="color: #800000; text-align: center; font-size: 50px; padding: 0px">
+        <h2>Login</h2>
+      </div>
+
+      <!-- Login box -->
+      <div class="formBox">
+        <form action="LoginFunction.php" method="post">
+          <div class="loginBoxes">
+            <label for="email"><b>Email Address</b></label>
+            <input type="text" placeholder="Enter your email address" name="email" required>
+
+            <label for="pword"><b>Password</b></label>
+            <input type="password" placeholder="Enter your password" name="pword" required>
+
+            <button type="submit">Login</button>
+          </div>
+      </div>
+
+      <div class="newUserBox">
+        <div class="box">
+          <p>New user? <a href="CreateAccount.php">Create An Account</a></p>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- Footer logic -->
+    <footer>
+      <div class="row">
+        <div class="column">
+          <p>We are dedicated to delivering a wide variety of products
+          made available by users.</p>
+        </div>
+        <div class="column">
+          <p>Home Office: Salt Lake City, Utah</p>
+          <p>Phone: 801-555-2323</p>
+        </div>
+      </div>
+    </footer>
+
+  </body>
+</html>
